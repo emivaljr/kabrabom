66be46f8a780bfff1a155d7a508f2e0eb0edda20
Emival Luiz da Silva Junior <emival@gmail.com>
2016-01-13 23:55:11 -0200

    Continue implementing user funcs

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :term,:cpf,:phone_number,:first_name,:last_name,:gender,:password_confirmation
-  attr_accessor :term
+  validates_presence_of :cpf,:phone_number,:first_name,:last_name,:gender
 end
